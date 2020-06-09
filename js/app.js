0bf9ca90eecd2179200df96adc7b7a78cd3606e5
Valentina-Olekhnovich <volekh@gmail.com>
2020-06-10 00:28:24 +0200

    Add button when the user scrolls

--- js/app.js
+++ js/app.js
@@ -65,10 +65,9 @@ window.onscroll = function() {
 
 // Build menu 
 
+
+
 // Scroll to section on link click
-
-// Set sections as active
-
 //Smooth scroll
 // materials from https://www.youtube.com/watch?v=oUSvlrDTLi4
 function smoothScroll(target, duration){
@@ -125,10 +124,9 @@ section3.addEventListener('click', (event) => {
     smoothScroll('#section3', 2000);
 });
 
-//Highlight section in viewport upon scrolling.
+// Highlight section in viewport upon scrolling.
 // Check if element is in the viewport
-
-
+// Set sections as active
 
 document.addEventListener('scroll', function(){
 
@@ -162,3 +160,25 @@ document.addEventListener('scroll', function(){
     });
 
 });
+
+//https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+//Button
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
