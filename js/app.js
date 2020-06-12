5df142b637fa6c4cc8186a1447abc0c40a24e0e5
Valentina-Olekhnovich <volekh@gmail.com>
2020-06-12 23:17:12 +0200

    Add menu in JS

--- js/app.js
+++ js/app.js
@@ -1,37 +1,34 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Define Global Variables
  *
-*/
-
+ */
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
-
-
+ *
+ */
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
 
@@ -50,124 +47,137 @@ window.onscroll = function() {
   prevScrollpos = currentScrollPos;
 }*/
 
-
 // Add class 'active' to section when near top of viewport
 
-
 // Scroll to anchor ID using scrollTO event
-
 
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
+// Build menu
 
-
-
-// Scroll to section on link click
-//Smooth scroll
-// materials from https://www.youtube.com/watch?v=oUSvlrDTLi4
-function smoothScroll(target, duration){
-    //Add target as a parameter. Target is a section you need.
-    var target = document.querySelector(target);
-    //Elements reletive position to the top of the screen. Use build in function
-    var targetPosition = target.getBoundingClientRect().top;
-    var startPosition = window.pageYOffset;
-    var distance = targetPosition - startPosition;
-    var startTime = null;
+const allSections = Array.from(document.querySelectorAll('section'));
+const menu = document.getElementById("navbar__list");
 
 
-    function animation(currentTime) {
-        if (startTime === null) startTime = currentTime;
-        var timeElapsed = currentTime - startTime;
-        var run = ease(timeElapsed, startPosition, targetPosition, duration);
-        // The First parameter is X - scroll horisontally, don't use 
-        window.scrollTo(0, run);
-        if(timeElapsed < duration) requestAnimationFrame (animation);
-    }
+allSections.forEach((section, index) => {
 
-// Function makes the scroll more interesting. Materials from https://www.gizma.com/easing/
-// Cubic easing in/out - acceleration until halfway, then deceleration
-// Replace Math.easeInOutCubic = function  on function ease
-    function ease (t, b, c, d) {
-        t /= d/2;
-        if (t < 1) return c/2*t*t*t + b;
-        t -= 2;
-        return c/2*(t*t*t + 2) + b;
-    }
+const newLi = document.createElement("li");
+const newA = document.createElement("a");
+const linkHref = `#section${index + 1}`;
+
+newA.setAttribute(`href`, linkHref);
+newA.innerHTML = `Link ${index + 1}`;
+
+newLi.classList = "menu__link";
+newLi.id = `nav_section${index + 1}`;
+
+newLi.appendChild(newA);
+menu.appendChild(newLi);
 
 
-    requestAnimationFrame(animation);
+
+})
+
+// Smooth scroll to section on link click
+// Materials from https://www.youtube.com/watch?v=oUSvlrDTLi4
+function smoothScroll(target, duration) {
+  // Add target as a parameter. Target is a section you need.
+  var target = document.querySelector(target);
+  // Elements reletive position to the top of the screen. Use build in function
+  var targetPosition = target.getBoundingClientRect().top;
+  var startPosition = window.pageYOffset;
+  var distance = targetPosition - startPosition;
+  var startTime = null;
+
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    var timeElapsed = currentTime - startTime;
+    var run = ease(timeElapsed, startPosition, targetPosition, duration);
+    // The First parameter is X - scroll horisontally, don't use
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
+
+  // Function makes the scroll more interesting. Materials from https://www.gizma.com/easing/
+  // Cubic easing in/out - acceleration until halfway, then deceleration
+  // Replace Math.easeInOutCubic = function  on function ease
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t * t + b;
+    t -= 2;
+    return (c / 2) * (t * t * t + 2) + b;
+  }
+
+  requestAnimationFrame(animation);
 }
-// Use  function call to test code   
+// Use function call to test code
 // smoothScroll('#section2', 1000)
 
-//Add event Listener REFACTOR LATER
-var section1 = document.querySelector ('#nav_section1');
-section1.addEventListener('click', (event) => {
-    event.preventDefault();
-    smoothScroll('#section1', 2000);
+// Add event Listener REFACTOR LATER
+var section1 = document.querySelector("#nav_section1");
+section1.addEventListener("click", (event) => {
+  event.preventDefault();
+  smoothScroll("#section1", 2000);
 });
 
-var section2 = document.querySelector ('#nav_section2');
-section2.addEventListener('click', (event) => {
-    event.preventDefault();
-    smoothScroll('#section2', 2000);
+var section2 = document.querySelector("#nav_section2");
+section2.addEventListener("click", (event) => {
+  event.preventDefault();
+  smoothScroll("#section2", 2000);
 });
 
-var section3 = document.querySelector ('#nav_section3');
-section3.addEventListener('click', (event) => {
-    event.preventDefault();
-    smoothScroll('#section3', 2000);
+var section3 = document.querySelector("#nav_section3");
+section3.addEventListener("click", (event) => {
+  event.preventDefault();
+  smoothScroll("#section3", 2000);
 });
 
 // Highlight section in viewport upon scrolling.
 // Check if element is in the viewport
 // Set sections as active
 
-document.addEventListener('scroll', function(){
+document.addEventListener("scroll", function () {
+  const allSections = Array.from(document.querySelectorAll("section"));
 
-    const allSections = Array.from(document.querySelectorAll('section'));
+  allSections.forEach(function (section, index) {
+    let domRect = section.getBoundingClientRect();
+    //console.log(window, innerHeight);
+    if (domRect.top < window.innerHeight - 150 && domRect.bottom > 150) {
+      //console.log("element is in viewport")
+      //console.log(index); - check which element is in viewport now
 
-    allSections.forEach(function(section, index){
-        let domRect = section.getBoundingClientRect(); 
-        //console.log(window, innerHeight);
-        if (domRect.top < window.innerHeight-150 && domRect.bottom > 150 ){
-            //console.log("element is in viewport")
-            //console.log(index); - check which element is in viewport now
-            
-            // Add class 'active' to section when near top of viewport
-            section.classList.add('your-active-class');
+      // Add class 'active' to section when near top of viewport
+      section.classList.add("your-active-class");
 
-            ////Highlight navigation menu link whan section in viewport upon scrolling.
-            let activeLink = document.getElementById(`nav_section${index + 1}`);
-            activeLink.classList.add('your-active-link');                    
-        }
+      //Highlight navigation menu link whan section in viewport upon scrolling.
+      let activeLink = document.getElementById(`nav_section${index + 1}`);
+      activeLink.classList.add("your-active-link");
+    } else {
+      //console.log("element is NOT in viewport")
 
-        else{
-            //console.log("element is NOT in viewport")
+      // Remove class 'active' from section when OUT of viewport
+      section.classList.remove("your-active-class");
 
-            // Remove class 'active' from section when OUT of viewport
-            section.classList.remove('your-active-class');
-
-            ////Remove highlight navigation menu link whan section is OUT of viewport upon scrolling.
-            let activeLink = document.getElementById(`nav_section${index + 1}`);
-            activeLink.classList.remove('your-active-link'); 
-        }
-    });
-
+      // Remove highlight navigation menu link whan section is OUT of viewport upon scrolling.
+      let activeLink = document.getElementById(`nav_section${index + 1}`);
+      activeLink.classList.remove("your-active-link");
+    }
+  });
 });
 
-//https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
-//Button
-//Get the button:
+// Materials from https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+// Button
+// Get the button:
 mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
